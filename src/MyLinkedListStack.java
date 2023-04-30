8a33d92a22688c7caf8ff33f87534f59559edf75
NackLAnk <www.at.7777@gmail.com>
2023-04-30 22:28:28 +0600

    MyLinkedListStack created

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,4 +1,12 @@
 import java.util.LinkedList;
-public class MyLinkedListStack {
-    private LinkedList<Integer> list; // instance variable of type LinkedList
+public class MyLinkedListStack<E> {
+    private LinkedList<E> stack; // instance variable of type LinkedList
+
+    /**
+     * Constructor to initialize the instance variable as a new empty LinkedList.
+     */
+    public MyLinkedListStack() {
+        stack = new LinkedList<>();
+    }
+
 }
